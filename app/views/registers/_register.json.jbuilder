50ec88d10d1d8474f241c9faaea509a5cdf344e2
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-05-18 08:56:12 +0545

    added pages controllers

--- app/views/registers/_register.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! register, :id, :username, :email, :password, :age, :created_at, :updated_at
-json.url register_url(register, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   resources :registers
   resources :logins
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'welcome#about'
+
+
   root to: "welcome#show"
+ 
+
+  get "about", to: "about#about"
+
+  get "service", to: "service#service"
+
+  get "contact", to: "contact#contact"
+ 
 
   # get "sign_in", to :"sign_in#login"
 
